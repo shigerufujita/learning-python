66b087e4d2ba2ab0f7575df721de4c37f95b6e91
Shigeru Fujita <shigeru.fujita@gmail.com>
2024-02-14 14:40:40 +0900

    script testing

---
+++ script_name.py
@@ -0,0 +1,8 @@
+# script_name.py
+# スクリプト名（今回はファイ名に等しい）
+
+def print_module():
+    print(__name__)
+
+if (__name__ == '__main__'):
+    print_module()
---
+++ script_use.py
@@ -0,0 +1,5 @@
+# script_use.py
+
+import script_name as sn
+
+sn.print_module()
