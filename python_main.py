3bd30e2aea869c9ad487d6df1d01ee631ce97053
Shigeru Fujita <shigerufujita@users.noreply.github.com>
2024-02-14 14:04:10 +0900

    Update python_main.py

--- python_main.py
+++ python_main.py
@@ -5,6 +5,6 @@ def greeting():
     print("Hello, World!")
 
 if __name__ == "__main__":
-    print(__name__) #__name__ は現在のモジュール名を保持する。モジュールとしてインポートされるとpython_mainとなる
+    print(__name__) # __name__ は現在のモジュール名を保持する。モジュールとしてインポートされるとpython_mainとなる
     main()          # しかし、スクリプトが直接実行されているときには、__name__は__main__となる。
                     # つまり、このファイルがモジュールとして使われる場合には、このif文は成立しない
